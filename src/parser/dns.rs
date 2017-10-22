0881448e602e0cbfc0e008260e2e9e9944c733b1
Kevin Brebanov <kevin.brebanov@gmail.com>
2017-10-22 10:52:23 -0400

    Parse DNS flags

--- src/parser/dns.rs
+++ src/parser/dns.rs
@@ -5,6 +5,7 @@ use serde_json::{Value, Map, Number};
 pub struct DnsHeader {
 	id: Number,
 	opcode: String,
+	flags: Value,
 	total_questions: Number,
 	total_answer_rrs: Number,
 	total_authority_rrs: Number,
@@ -21,9 +22,30 @@ impl DnsHeader {
 			_ => "UNKNOWN",
 		};
 
+		let mut flags: Vec<&str> = Vec::new();
+		if p.header.authoritative {
+			flags.push("AA");
+		}
+		if p.header.truncated {
+			flags.push("TC");
+		}
+		if p.header.recursion_desired {
+			flags.push("RD");
+		}
+		if p.header.recursion_available {
+			flags.push("RA");
+		}
+		if p.header.authenticated_data {
+			flags.push("AD");
+		}
+		if p.header.checking_disabled {
+			flags.push("CD");
+		}
+
 		DnsHeader {
 			id: Number::from(p.header.id),
 			opcode: opcode_string.to_string(),
+			flags: Value::from(flags),
 			total_questions: Number::from(p.header.questions),
 			total_answer_rrs: Number::from(p.header.answers),
 			total_authority_rrs: Number::from(p.header.nameservers),
@@ -36,6 +58,7 @@ impl DnsHeader {
 
 		header.insert("id".to_string(), Value::Number(self.id.clone()));
 		header.insert("opcode".to_string(), Value::String(self.opcode.clone()));
+		header.insert("flags".to_string(), self.flags.clone());
 		header.insert("total_questions".to_string(), Value::Number(self.total_questions.clone()));
 		header.insert("total_answer_rrs".to_string(), Value::Number(self.total_answer_rrs.clone()));
 		header.insert("total_authority_rrs".to_string(), Value::Number(self.total_authority_rrs.clone()));
