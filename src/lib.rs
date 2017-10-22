56340e7d7ff3a8032436b6afb9f14401a7997ad6
Kevin Brebanov <kevin.brebanov@gmail.com>
2017-10-22 12:48:33 -0400

    Parse DNS questions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 extern crate pcap;
 extern crate pnet;
 extern crate dns_parser;
+extern crate serde;
 extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
 
 mod parser;
 pub mod sniffer;
--- src/parser/dns.rs
+++ src/parser/dns.rs
@@ -1,6 +1,13 @@
 use dns_parser::Packet as DnsPacket;
-use dns_parser::{Opcode, ResponseCode};
-use serde_json::{Value, Map, Number};
+use dns_parser::{Opcode, ResponseCode, QueryType, QueryClass};
+use serde_json::{Value, Map, Number, to_value};
+
+#[derive(Serialize)]
+pub struct DnsQuestion {
+	name: String,
+	qtype: String,
+	qclass: String,
+}
 
 pub struct DnsHeader {
 	id: Number,
@@ -11,6 +18,7 @@ pub struct DnsHeader {
 	total_answer_rrs: Number,
 	total_authority_rrs: Number,
 	total_additional_rrs: Number,
+	questions: Value,
 }
 
 impl DnsHeader {
@@ -54,6 +62,46 @@ impl DnsHeader {
 			_ => "UNKNOWN",
 		};
 
+		let mut questions: Vec<DnsQuestion> = Vec::new();
+		if ! p.questions.is_empty() {
+			for q in p.questions.iter() {
+				let question = DnsQuestion {
+					name: q.qname.to_string(),
+					qtype: match q.qtype {
+						QueryType::A => String::from("A"),
+						QueryType::NS => String::from("NS"),
+						QueryType::MF => String::from("MF"),
+						QueryType::CNAME => String::from("CNAME"),
+						QueryType::SOA => String::from("SOA"),
+						QueryType::MB => String::from("MB"),
+						QueryType::MG => String::from("MG"),
+						QueryType::MR => String::from("MR"),
+						QueryType::NULL => String::from("NULL"),
+						QueryType::WKS => String::from("WKS"),
+						QueryType::PTR => String::from("PTR"),
+						QueryType::HINFO => String::from("HINFO"),
+						QueryType::MINFO => String::from("MINFO"),
+						QueryType::MX => String::from("MX"),
+						QueryType::TXT => String::from("TXT"),
+						QueryType::AAAA => String::from("AAAA"),
+						QueryType::SRV => String::from("SRV"),
+						QueryType::AXFR => String::from("AXFR"),
+						QueryType::MAILB => String::from("MAILB"),
+						QueryType::MAILA => String::from("MAILA"),
+						QueryType::All => String::from("ALL"),
+					},
+					qclass: match q.qclass {
+						QueryClass::IN => String::from("IN"),
+						QueryClass::CS => String::from("CS"),
+						QueryClass::CH => String::from("CH"),
+						QueryClass::HS => String::from("HS"),
+						QueryClass::Any => String::from("ANY"),
+					},
+				};
+				questions.push(question);
+			}
+		}
+
 		DnsHeader {
 			id: Number::from(p.header.id),
 			opcode: opcode_string.to_string(),
@@ -63,6 +111,7 @@ impl DnsHeader {
 			total_answer_rrs: Number::from(p.header.answers),
 			total_authority_rrs: Number::from(p.header.nameservers),
 			total_additional_rrs: Number::from(p.header.additional),
+			questions: to_value(questions).unwrap(),
 		}
 	}
 
@@ -77,6 +126,7 @@ impl DnsHeader {
 		header.insert("total_answer_rrs".to_string(), Value::Number(self.total_answer_rrs.clone()));
 		header.insert("total_authority_rrs".to_string(), Value::Number(self.total_authority_rrs.clone()));
 		header.insert("total_additional_rrs".to_string(), Value::Number(self.total_additional_rrs.clone()));
+		header.insert("questions".to_string(), self.questions.clone());
 
 		header
 	}
