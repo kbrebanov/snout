ceeefd1058565f0d655513958d819ac4a9131bb0
Kevin Brebanov <kevin.brebanov@gmail.com>
2017-10-23 14:31:33 -0400

    Use pcap streams

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ extern crate pnet;
 extern crate dns_parser;
 extern crate serde;
 extern crate serde_json;
+extern crate futures;
+extern crate tokio_core;
 
 #[macro_use]
 extern crate serde_derive;
--- src/sniffer/mod.rs
+++ src/sniffer/mod.rs
@@ -1,22 +1,43 @@
-use pcap::Capture;
+use pcap::{Capture, Packet, Error};
+use pcap::tokio::PacketCodec;
 use serde_json;
 use parser;
+use tokio_core::reactor::Core;
+use futures::stream::Stream;
+
+struct JsonDumpCodec {}
+
+impl PacketCodec for JsonDumpCodec {
+    type Type = String;
+
+    fn decode<'p>(&mut self, packet: Packet<'p>) -> Result<Self::Type, Error> {
+        let packet_headers = parser::parse_headers(packet);
+        let p = serde_json::to_string(&packet_headers).unwrap();
+        Ok(p)
+    }
+}
 
 pub fn sniff(interface: &str, promiscuous: bool, snaplen: i32, timeout: i32, filter: &str) {
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
     let mut capture = Capture::from_device(interface).unwrap()
                       	.promisc(promiscuous)
                         .snaplen(snaplen)
                         .timeout(timeout)
                         .open()
+                        .unwrap()
+                        .setnonblock()
                         .unwrap();
 
     if !filter.is_empty() {
     	capture.filter(filter).unwrap();
     }
 
-    while let Ok(packet) = capture.next() {
-    	let packet_headers = parser::parse_headers(packet);
-		  println!("{}", serde_json::to_string(&packet_headers).unwrap());
-    }
-    println!();
+    let s = capture.stream(&handle, JsonDumpCodec{}).unwrap();
+    let done = s.for_each(move |s| {
+        println!("{}", s);
+        Ok(())
+    });
+
+    core.run(done).unwrap();
 }
